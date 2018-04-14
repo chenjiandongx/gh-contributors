3ca9526cb5dd4eead02864f70a98e8b150becd16
chenjiandongx <chenjiandongx@qq.com>
2018-04-14 11:19:59 +0800

    测试 readme

--- __init__.py
+++ __init__.py
@@ -8,9 +8,10 @@ class Contributor:
 
     @property
     def avatar(self):
-        return "![{name}]({url}) |".format(
-            name=self.contributor["login"],
-            url=self.contributor["avatar_url"]
+        return '<img src="{src}" alt="{alt}" width="{size}px" height="{size}px"/> |'.format(
+            alt=self.contributor["login"],
+            src=self.contributor["avatar_url"],
+            size=100
         )
 
     @property
@@ -21,19 +22,31 @@ class Contributor:
         )
 
 
-def save(url):
+def get_res(res, avatars, guys, split):
+    avatars.append("\n")
+    split.append("\n")
+    res += "".join(avatars + split + guys) + "\n\n"
+    return res
+
+
+def save(url, cnt=6):
+
     req = requests.get(url).json()
     avatars, guys, split = ["| "], ["| "], ["| "]
+    res = ""
 
-    for r in req:
+    for index, r in enumerate(req):
         c = Contributor(r)
         avatars.append(c.avatar)
         split.append(":----: |")
         guys.append(c.guy)
 
-    avatars.append("\n")
-    split.append("\n")
+        if (index + 1) % cnt == 0:
+            res = get_res(res, avatars, guys, split)
+            avatars, guys, split = ["| "], ["| "], ["| "]
 
-    res = "".join(avatars + split + guys)
+    res = get_res(res, avatars, guys, split)
     with open("README.md", mode="w+", encoding="utf8") as f:
         f.write(res)
+
+save("https://api.github.com/repos/pyecharts/pyecharts/contributors")
